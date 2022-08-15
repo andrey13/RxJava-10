59e981c8fd65fd423b5dd973d9777f374e7d1868
Andrey Krashenitza <andrey13@yandex.ru>
2022-08-15 16:38:27 +0300

    p200 getting categories

--- app/src/main/java/com/raywenderlich/android/ourplanet/EventAdapter.kt
+++ app/src/main/java/com/raywenderlich/android/ourplanet/EventAdapter.kt
@@ -1,33 +1,3 @@
-/*
- * Copyright (c) 2020 Razeware LLC
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
- * distribute, sublicense, create a derivative work, and/or sell copies of the 
- * Software in any work that is designed, intended, or marketed for pedagogical or 
- * instructional purposes related to programming, coding, application development, 
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works, 
- * or sale is expressly withheld.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package com.raywenderlich.android.ourplanet
 
 import androidx.recyclerview.widget.RecyclerView
@@ -37,7 +7,8 @@ import android.widget.TextView
 import com.raywenderlich.android.ourplanet.model.EOEvent
 import com.raywenderlich.android.ourplanet.model.EONET
 
-class EventAdapter(private val events: MutableList<EOEvent>) : RecyclerView.Adapter<EventAdapter.ViewHolder>() {
+class EventAdapter(private val events: MutableList<EOEvent>)
+  : RecyclerView.Adapter<EventAdapter.ViewHolder>() {
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
     return ViewHolder(parent.inflate(R.layout.list_item_event))
@@ -77,3 +48,4 @@ class EventAdapter(private val events: MutableList<EOEvent>) : RecyclerView.Adap
     }
   }
 }
+
